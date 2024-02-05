d13986eabd2c1a97222dede50927a02bdb9896b1
Abeh995 <a.beh2019.3@gmail.com>
2024-02-05 18:18:03 +0330

    Phase 3

--- Quoridor.c
+++ Quoridor.c
@@ -5,11 +5,14 @@
 #include <stdbool.h>
 //#include <ctime.h>
 #include <time.h>
+#include <string.h>
+
+#define wallsize 15 
 
 char A[100][100];
 char B[100][100];
 int playloc[2][4];
-int walloc[15][15]; // jahat zakire charRah ha
+int walloc[wallsize][wallsize]; // jahat zakhire charRah ha // ARRAY START FROM 1
 
 typedef enum {
   BLACK = 0,
@@ -84,16 +87,16 @@ void winner(int *sw,int n,char player1,char player2,char player3,char player4,in
 		printf("HERE WE ARE...\n");
 		sleep(3500);
 		if(r1==2*n-1){
-		printf("  **\n ****\n******\n %c WON THE MATCH\n",player1);
+		printf("  **\n ****\n******\n %s WON THE MATCH\n",player1);
 		}
 		else if(c2==1){
-		printf("  **\n ****\n******\n %c WON THE MATCH\n",player2);
+		printf("  **\n ****\n******\n %s WON THE MATCH\n",player2);
 		}
 		else if(r3==1){
-		printf("  **\n ****\n******\n %c WON THE MATCH\n",player3);
+		printf("  **\n ****\n******\n %s WON THE MATCH\n",player3);
 		}
 		else{
-		printf("  **\n ****\n******\n %c WON THE MATCH\n",player4);
+		printf("  **\n ****\n******\n %s WON THE MATCH\n",player4);
 		}
 	}
 }
@@ -200,8 +203,11 @@ int Wall(char A[][100],char B[][100],int n,int wi,int wj, int hv,int r1,int c1,i
 	int i,j;
 	int m=2*n+1;
 	bool can = false ;
-	if(wi>n-1|| wj >n-1){
+	if(wi>n-1|| wj >n-1){ // BEHINE SAZI SHAVAD
 		return 1; //OUT OF RULE
+	}
+	if(wi<1||wj<1){
+		return 1;//OUT OF RULE
 	}
 	if(hv!=1 && hv!=2){
 		return 1;//OUT OF RULE
@@ -216,6 +222,7 @@ int Wall(char A[][100],char B[][100],int n,int wi,int wj, int hv,int r1,int c1,i
 	wj*=2;
 	if(hv==1){// 1 mean: OFOGHI
 		if(A[wi][wj-1]==42 ||A[wi][wj+1]==42){// CHAHAR RAH POR  AST LOTFAN BADAN TAMAS BEGIRID
+			walloc[wi/2][wj/2]=0;
 			return 1; //OUT OF RULE
 		}
 		else{// build walls
@@ -228,12 +235,14 @@ int Wall(char A[][100],char B[][100],int n,int wi,int wj, int hv,int r1,int c1,i
 			else{
 				A[wi][wj-1]='-';
 				A[wi][wj+1]='-';
+				walloc[wi/2][wj/2]=0;
 				return 1; // OUT OF RULE
 			}
 		}
 	}
 	else{// hv=2 AMOODI
 		if(A[wi-1][wj]=='*' ||A[wi+1][wj]=='*' ){// CHAHAR RAH POR  AST LOTFAN BADAN TAMAS BEGIRID
+			walloc[wi/2][wj/2]=0;
 			return 1;//OUT OF RULE
 		}
 		else{//build walls
@@ -246,6 +255,7 @@ int Wall(char A[][100],char B[][100],int n,int wi,int wj, int hv,int r1,int c1,i
 			else{
 				A[wi-1][wj]='|';
 				A[wi+1][wj]='|';
+				walloc[wi/2][wj/2]=0;
 				return 1; //OUT OF RULE
 			}
 		}
@@ -520,32 +530,104 @@ void printb(int m){
 
 int main()
 {   
+	srand(time(NULL));
 	printf("Welcome to Iran\n");
-    int n,m,i,j,rows,cols;
-    int k  ,r1,c1,r2,c2,r3,c3,r4,c4  ;// r1: PLayer 1 row & .... 
-    int wall, wmove, plan;
-    int swWIN=1,swWall=1,sw, sw1,sw2;
-	int turn=1;
-	int r,c;
-	int wi,wj,hv; // wall location & ofoghi ya amoodi
-	int p1wc,p2wc,p3wc,p4wc;// players wall counter: number of available wall of each player
-	
-	
+	//printf("%d",rand());
+	//printf("\n%d",rand());
+
 	char player1[21];
 	char player2 [21];
 	char player3[21];
 	char player4[21];
-	printf("Player 1 name: ");
-	scanf("%s",player1);
-	printf("Player 2 name: ");
-	scanf("%s",player2);
-	printf("Player 3 name: ");
-	scanf("%s",player3);
-	printf("Player 4 name: ");
-	scanf("%s",player4);
+	
+    int n,m,i,j,rows,cols;
+    int k  ,r1,c1,r2,c2,r3,c3,r4,c4  ;// r1: PLayer 1 row & .... 
+    int wall, wmove, plan, random,random2, random3;
+    int swWIN=1,swWall=1,sw=1, sw1,sw2, swRobot=0,swSucs;
+	int turn=1;
+	int r,c;
+	int NewGame, BotGame;
+	int wi,wj,hv; // wall location & ofoghi ya amoodi
+	int p1wc,p2wc,p3wc,p4wc;// players wall counter: number of available wall of each player
+	bool P1_is_bot,P2_is_bot,P3_is_bot,P4_is_bot;
+	P1_is_bot=P2_is_bot=P3_is_bot=P4_is_bot=false;
+	// LOADING ...
+
+	clear();
+	printf("\n   MAIN MENU\n\n\n   *New Game: 1\n\n   *Load Game: 2\n    : ");
+	scanf("%d",&NewGame);
+	while(1){
+		if(NewGame==1) {
+			clear();
+			
+			printf("\n   *Play With ROBOTS: 1\n\n   *PLAY UR OWN: 2\n    : ");
+			scanf("%d",&BotGame);
+			while(1){
+				if(BotGame==1){// YES, PLAY WITH ROBOTS
+					P2_is_bot=true;
+					P3_is_bot=true;
+					P4_is_bot=true;
+					clear();
+					break;
+				}
+				else if(BotGame==2){ // NO, PLAY WITHOUT ROBOTS
+					
+					// EXCEPT 2PLAYER MODE
+					break;
+				}
+				printf("WORNG\aENTER 1 FOR PLAY WITH BOTS or ENTER 2 FOR PLAY YOUR OWN\nPLEASE ENTER AGAIN:\n");
+				scanf("%d",&BotGame);
+			}
+			sw=1;
+			
+			break;
+		}
+		else if(NewGame==2){// LOAD GAME
+			sw=0;
+			clear();
+			
+			break;
+		}
+		printf("Worng!\a \nEnter 1 for New Game or 2 for Load Game: ");
+		scanf("%d",&NewGame);
+	}
+	// 4 ta string and 4 ta int (wall) AND int(n*n) AND 8 ta int( loaction r1 c1)
+	//  (n-1 * n-1)AND (HV) ta  int (wall location)  AND  TURNNNNNN
+	
+	if(P1_is_bot==false){ //REAL PLAYER
+		printf("Player 1 name: ");
+		scanf("%s",player1);
+	}
+	else{
+		player1[5]="Alex";
+	}
+	
+	if(P2_is_bot==false){ //REAL PLAYER
+		printf("Player 2 name: ");
+		scanf("%s",player2);
+	}
+	else{
+		strcpy(player2,"Charlotte");
+	}
+	
+	if(P3_is_bot==false){ //REAL PLAYER
+		printf("Player 3 name: ");
+		scanf("%s",player3);
+	}
+	else{
+		strcpy(player3,"Jia");
+	}
+	
+	if(P4_is_bot==false){ //REAL PLAYER
+		printf("Player 4 name: ");
+		scanf("%s",player4);
+	}
+	else{
+		strcpy(player4,"Gabriel");
+	}
 	
 	while(1){
-    	printf("please enter number of Dimensions:\n");
+    	printf("Please enter number of Dimensions:\n");
     	scanf("%d",&n);
     	if(n>=3 && n<=25) break;
 	}
@@ -587,11 +669,18 @@ int main()
 		r4=n-1;
 		c4=1;
 	}
-		
+	// 
+	for(i=0;i<wallsize;i++){
+		walloc[i][0]=1;
+	}
+	for(j=0;j<wallsize;j++){
+		walloc[0][j]=1;
+	}
+	// i=0;j=0;
 	while(1){
 		printf("Please enter Number of Walls:");
 		scanf("%d",&wall);
-		if(wall>=0&&wall<=20) break;
+		if(wall>=1&&wall<=20) break;
 		else{
 			printf("WRONG ANSWER\n Enter again\a");
 		}
@@ -606,109 +695,216 @@ int main()
 			c=c1;
 			setColor(3,0);
 			printf("%s's turn.TURN:[%d]\n",player1,turn);
+			if(P1_is_bot){
+				swRobot=1;
+			}
 		}
 		else if(turn%4==2){//PLAYER 2 TURN
 			r=r2;
 			c=c2;
 			setColor(5,0);
 			printf("%s's turn.TURN:[%d]\n",player2,turn);
+			if(P2_is_bot){
+				swRobot=1;
+			}
 		}
 		else if(turn%4==3){ //PLAYER 3 TURN
 			r=r3;
 			c=c3;
 			setColor(8,0);
 			printf("%s's turn.TURN:[%d]\n",player3,turn);
+			if(P3_is_bot){
+				swRobot=1;
+			}
 		}
 		else{ // PLAYER 4 TURN
 			r=r4;
 			c=c4;
 			setColor(2,0);
 			printf("%s's turn.TURN:[%d]\n",player4,turn);
+			if(P4_is_bot){
+				swRobot=1;
+			}
 		}
 		print(m);
-		printf("What's the plan?\nMOVE (1) Or WALL (2)? ( Enter num (1) or (2) ) :");
-		sw=0;
-		while(sw==0){
-			scanf("%d",&plan);
-			if(plan==1||plan==2)sw=1;
-			else{printf("Wrong plan\a Enter again:");
-			}
-		}
-		if(plan==1){
-			printf("Enter number of move up(8) Or down(2) Or left(4) Or right(6):");
+		
+		if(swRobot==0){// real player's turn
+			printf("What's the plan?\nMOVE (1) Or WALL (2)? ( Enter num (1) or (2) ) :");
 			while(1){
-				scanf("%d",&wmove);
-				if(wmove==8||wmove==2||wmove==4||wmove==6) break;
-				printf("Wrong MOVE\a enter again:");
-			}
-			while(1){
-				if(Move( A ,n,wmove,&r,&c)==1){// "1" mean: can't move to that location (OUT OF RULE)
-					printf("out of rule\a Enter another:");
-					scanf("%d",&wmove);
-				}
-				else {// SUCCESS MOVE
-					if(turn%4==1){ // LAYER 1 TURN
-						r1=r;
-						c1=c;
-					}
-					else if(turn%4==2){// PLAYER 2 TURN
-						r2=r;
-						c2=c;
-					}
-					else if(turn%4==3){//PLAYER 3 TURN
-						r3=r;
-						c3=c;
-					}
-					else{// PLAYER 4 TURN
-						r4=r;
-						c4=c;
-					}
-					clear();
-					print(m);
-					break;
+				scanf("%d",&plan);
+				if(plan==1||plan==2) break;
+				else{printf("Wrong plan\a Enter again:");
 				}
 			}
-			
-			winner(&swWIN,n,player1,player2,player3,player4,r1,c2,r3,c4);
-		}
-		else{// wall
-			while(swWall){// baraye break bad az gozashtan wall
-				if(p1wc==0||p2wc==0){// one of players have no wall...
-					if(turn%2==1&& p1wc==0){
-						printf("you have not enogh wall\a\nplease move ");
-						turn--;
-						break;
-					}
-					else if(turn%2==0&& p2wc==0){
-						printf("U have not enogh wall\a\nplease move ");
-						turn--;
-						break;
-					}
-				}
-				printf("Please enter location of FORWAY (4 raah) :)\nenter i,j,h (1) OR v (2): ");
-				scanf("%d %d %d",&wi,&wj,&hv);
+			if(plan==1){ // move
+				printf("Enter number of move up(8) Or down(2) Or left(4) Or right(6):");
 				while(1){
-					if( Wall(A,B,n,wi,wj,hv,r1,c1,r2,c2,r3,c3,r4,c4)==1){
-						printf("out of RULE, enter again:\a\n ");
-						scanf("%d %d %d",&wi,&wj,&hv);
+					scanf("%d",&wmove);
+					if(wmove==8||wmove==2||wmove==4||wmove==6) break;
+					printf("Wrong MOVE\a enter again:");
+				}
+				while(1){
+					if(Move( A ,n,wmove,&r,&c)==1){// "1" mean: can't move to that location (OUT OF RULE)
+						printf("out of rule\a Enter another:");
+						scanf("%d",&wmove);
 					}
-					else{// SUCCESS
-						if(turn%2==1){
-							p1wc--;
+					else {// SUCCESS MOVE
+						if(turn%4==1){ // LAYER 1 TURN
+							r1=r;
+							c1=c;
 						}
-						else{
-							p2wc--;
+						else if(turn%4==2){// PLAYER 2 TURN
+							r2=r;
+							c2=c;
 						}
-						//setColor(4,0);
-						swWall=0;//
+						else if(turn%4==3){//PLAYER 3 TURN
+							r3=r;
+							c3=c;
+						}
+						else{// PLAYER 4 TURN
+							r4=r;
+							c4=c;
+						}
+						clear();
+						print(m);
 						break;
 					}
 				}
+				
+				winner(&swWIN,n,player1,player2,player3,player4,r1,c2,r3,c4);
 			}
-			swWall=1;
+			else if(plan==2){// wall
+				while(swWall){// baraye break bad az gozashtan wall
+					if(p1wc==0||p2wc==0){// one of players have no wall...
+						if(turn%4==1&& p1wc==0){
+							printf("you have not enogh wall\a\nplease move\n");
+							turn--;
+							break;
+						}
+						else if(turn%4==2&& p2wc==0){
+							printf("U have not enogh wall\a\nplease move\n");
+							turn--;
+							break;
+						}
+					}
+					else if(p3wc==0||p4wc==0){
+						if(turn%4==3&&p3wc==0){
+							printf("YOU HAVE NOT ENOGH WALL\a\nPLEASE MOVE\n");
+							turn--;
+							break;
+						}
+						else if(turn%4==0&&p4wc==0){
+							printf("U HAVE NOT ENOGH WALL\a\nPLEASE MOVE\n");
+							turn--;
+							break;
+						}
+					}
+					printf("Please enter location of FORWAY (4 raah) :)\nenter i,j,h (1) OR v (2): ");
+					scanf("%d %d %d",&wi,&wj,&hv);
+					while(1){
+						if( Wall(A,B,n,wi,wj,hv,r1,c1,r2,c2,r3,c3,r4,c4)==1){
+							printf("out of RULE, enter again:\a\n ");
+							scanf("%d %d %d",&wi,&wj,&hv);
+						}
+						else{// SUCCESS
+							if(turn%4==1){
+								p1wc--;
+							}
+							else if(turn%4==2){
+								p2wc--;
+							}
+							else if(turn%4==3){
+								p3wc--;
+							}
+							else{
+								p4wc--;
+							}
+								//setColor(4,0);
+							swWall=0;//
+							break;
+						}
+					}
+				}
+				//swWall=1;
+			}
+		}
+		else{//   ROBOT'S TURN
+			sleep(2000);
+			// random plan
+			if(rand()%2==1){// MOVE
+				plan=1;
+				while(1){
+					random=rand()%4;
+					if(random==1) wmove=8;
+					else if(random==2) wmove=2;
+					else if(random==3) wmove=4;
+					else if(random==0)wmove=6;
+					if(Move( A ,n,wmove,&r,&c)==0){ // SUCCESS MOVED.
+						if(turn%4==1){
+							r1=r;
+							c1=c;
+						}
+						else if(turn%4==2){
+							r2=r;
+							c2=c;
+						}
+						else if(turn%4==3){
+							r3=r;
+							c3=c;
+						}
+						else {
+							r4=r;
+							c4=c;
+						}
+						break;
+					}
+				}
+				// BASTE BE TURN R1 C1 .....
+				
+				winner(&swWIN,n,player1,player2,player3,player4,r1,c2,r3,c4);
+			}
+			else{// WALL
+				plan=2;
+				if(turn%4==1&&p1wc==0){
+					turn--;
+					swSucs=0;
+				}
+				else if(turn%4==2&&p2wc==0){
+					turn --;
+					swSucs=0;
+				}
+				else if(turn%4==3&&p3wc==0){
+					turn--;
+					swSucs=0;
+				}
+				else if(p4wc==0){
+					turn--;
+					swSucs=0;
+				}
+				while(swSucs){
+					random=rand()%n;
+					random2=rand()%n;
+					if(walloc[random][random2]==0){
+						while(1){
+							random3=rand()%3;
+							if(random3==0)random3++;
+							if( Wall(A,B,n,random,random2,random3,r1,c1,r2,c2,r3,c3,r4,c4)==0){
+								if(turn%4==1) p1wc--;
+								else if(turn%4==2) p2wc--;
+								else if( turn%4==3) p3wc--;
+								else p4wc--;
+								swSucs=0;
+								break;
+							}
+						}
+					}
+				}
+			}
 		}
 		turn++;// PLAYERS TURN
 		swWall=1;
+		swSucs=1;
+		swRobot=0;
 	}
 	
 	return 0;
